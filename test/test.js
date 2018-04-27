3f0a688fdb8dff04fd0f1116a588ec235ffe5fb1
Elise Dettrick <elise@dettrick.net>
2018-04-27 12:06:53 +1000

    Add a test to check that moon times are Dates

--- test/test.js
+++ test/test.js
@@ -27,5 +27,20 @@ describe("SunCalc", function() {
       expect(moonTimes).to.have.property("rise");
       expect(moonTimes).to.have.property("set");
     });
+
+    it("should return moon rise and set as Dates", function() {
+      let input = {
+        date: "2018/04/26",
+        latitude: -27,
+        longitude: 153
+      };
+      let moonTimes = sunCalc.getMoonTimes(
+        dateParser(input.date),
+        input.latitude,
+        input.longitude
+      );
+      expect(moonTimes.rise).to.be.a("date");
+      expect(moonTimes.set).to.be.a("date");
+    });
   });
 });
