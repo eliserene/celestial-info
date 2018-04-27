5121995a687362ff990e50142eae8ccbc9a164f7
Elise Dettrick <elise@dettrick.net>
2018-04-27 12:05:08 +1000

    Add my first test for moon rise and set times

--- test/test.js
+++ test/test.js
@@ -7,3 +7,25 @@ describe("Array", function() {
     });
   });
 });
+
+var sunCalc = require("suncalc"); //library for calcuating sun/moon positions and phases
+var dateParser = require("date-fns/parse");
+
+describe("SunCalc", function() {
+  describe("moon times", function() {
+    it("should return moon rise and moon set", function() {
+      let input = {
+        date: "2018/04/26",
+        latitude: -27,
+        longitude: 153
+      };
+      let moonTimes = sunCalc.getMoonTimes(
+        dateParser(input.date),
+        input.latitude,
+        input.longitude
+      );
+      expect(moonTimes).to.have.property("rise");
+      expect(moonTimes).to.have.property("set");
+    });
+  });
+});
