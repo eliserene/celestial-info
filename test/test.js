bcd41112d34a52c687f38dd7f5effc6165adac8c
Elise Dettrick <elise@dettrick.net>
2018-04-27 12:09:44 +1000

    Refactor tests to remove duplication

--- test/test.js
+++ test/test.js
@@ -12,33 +12,28 @@ var sunCalc = require("suncalc"); //library for calcuating sun/moon positions an
 var dateParser = require("date-fns/parse");
 
 describe("SunCalc", function() {
+  let moonTimes = {};
+  before(function() {
+    // runs before all tests in this block
+    let input = {
+      date: "2018/04/26",
+      latitude: -27,
+      longitude: 153
+    };
+    moonTimes = sunCalc.getMoonTimes(
+      dateParser(input.date),
+      input.latitude,
+      input.longitude
+    );
+  });
+
   describe("moon times", function() {
     it("should return moon rise and moon set", function() {
-      let input = {
-        date: "2018/04/26",
-        latitude: -27,
-        longitude: 153
-      };
-      let moonTimes = sunCalc.getMoonTimes(
-        dateParser(input.date),
-        input.latitude,
-        input.longitude
-      );
       expect(moonTimes).to.have.property("rise");
       expect(moonTimes).to.have.property("set");
     });
 
     it("should return moon rise and set as Dates", function() {
-      let input = {
-        date: "2018/04/26",
-        latitude: -27,
-        longitude: 153
-      };
-      let moonTimes = sunCalc.getMoonTimes(
-        dateParser(input.date),
-        input.latitude,
-        input.longitude
-      );
       expect(moonTimes.rise).to.be.a("date");
       expect(moonTimes.set).to.be.a("date");
     });
