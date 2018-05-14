cba5faaa109ff6dbebb5fbc30989f8c1f10e7059
Elise Dettrick <elise@dettrick.net>
2018-05-14 21:55:12 +1000

    Moving router back to app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 const express = require("express"); // call express
 const app = express(); // define our app using express
 const bodyParser = require("body-parser");
-const routes = require("./router.js");
+const handlers = require("./handler.js");
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,9 +18,18 @@ var port = process.env.PORT || 5000; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
+const router = express.Router();
+
+// test route to make sure everything is working
+//(accessed at GET http://localhost:5000/api)
+router.get("/", function(req, res) {
+  res.json({ message: "hooray! welcome to our api!" });
+});
+router.get("/moon/times/:latitude/:longitude/:date", handlers.MoonRiseSet);
+router.get("/moon/phase/:date", handlers.MoonPhase);
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use("/api", routes);
+app.use("/api", router);
 
 // START THE SERVER
 // =============================================================================
