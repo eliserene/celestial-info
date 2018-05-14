9c2ef4f16da6c974bc00cfa3e89f19ef5108d099
Elise Dettrick <elise@dettrick.net>
2018-05-14 22:42:05 +1000

    Convert to webfunc

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-// app.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-const express = require("express"); // call express
-const app = express(); // define our app using express
-const bodyParser = require("body-parser");
+const { app } = require("webfunc");
 const handlers = require("./handler.js");
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 var port = process.env.PORT || 5000; // set our port
 
 // ROUTES FOR OUR API
-// =============================================================================
-const router = express.Router();
-
-// test route to make sure everything is working
-//(accessed at GET http://localhost:5000/api)
-router.get("/", function(req, res) {
+app.get("/api/", function(req, res) {
   res.json({ message: "hooray! welcome to our api!" });
 });
-router.get("/moon/times/:latitude/:longitude/:date", handlers.MoonRiseSet);
-router.get("/moon/phase/:date", handlers.MoonPhase);
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use("/api", router);
+app.get("/api/moon/times/:latitude/:longitude/:date", handlers.MoonRiseSet);
+app.get("/api/moon/phase/:date", handlers.MoonPhase);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log("Magic happens on port " + port);
+eval(app.listen("app", port));
